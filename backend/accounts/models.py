692e8e382c8cad617d1497ad51799cc9cf1466d4
Aditya Dubey <adityadubey362@gmail.com>
2026-02-06 11:19:22 +0530

    Added user model and admin for backend

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-
 # Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('candidate','Candidate'),
+        ('recruiter','Recruiter'),
+        ('admin','Admin')
+    )
+    email =models.EmailField(unique=True)
+    role = models.CharField(max_length=50,choices=ROLE_CHOICES)
+    
+    
+    USERNAME_FIELD ='email'
+    REQUIRED_FIELDS = ['username']
+    
+    def __str__(self):
+        return self.email
